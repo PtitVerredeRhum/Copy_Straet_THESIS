79160e7a0c66c7e52e08eb41d8d11c14c77d2a0e
CLOUX Romain <romain.cloux@live.be>
2024-05-29 22:31:42 +0200

    writegdx...

--- data-generation/sampling.py
+++ data-generation/sampling.py
@@ -183,7 +183,7 @@ def prepare_simulation_files(sample, cur_folder):
     data = ds.adjust_capacity(data, ('WTON','WIN'),
                             value=(peak_load*FC_load)/CF_wton*(share_wind - ref_values['share_wind_off']), singleunit=True)
     data = ds.adjust_capacity(data, ('PHOT','SUN'),
-                            value=(peak_load*FC_load)*share_pv/CF_pv, singleunit=True,dest_path=cur_folder, temp_path=tmp)
+                            value=(peak_load*FC_load)*share_pv/CF_pv, singleunit=True,write_gdx=True,dest_path=cur_folder, temp_path=tmp)
     
     
 
